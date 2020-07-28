fc560bb9e3080a0662ea834b56a68c3c5646a1f8
Manish Rawat <62084296+githubjmtss@users.noreply.github.com>
2020-07-28 11:40:53 +0530

    simple dixe rolling website

---
+++ hello.js
@@ -0,0 +1,10 @@
+function hellos(){
+    let  picname = "dice";
+    let extension=".png";
+    let x = Math.floor((Math.random() * 5) + 1);
+    let y = Math.floor((Math.random() * 5) + 1);
+    let name1 = "img/" + picname + x + extension;
+    let name2 = "img/" + picname + y + extension;
+    document.getElementById('favcon1').src = name1;
+    document.getElementById('favcon2').src = name2;
+}
